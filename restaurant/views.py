e9a4f8ff51beb3bf2932915cb8496593a77c7e9e
Dareach Hoeurn <dareachhoeurn@gmail.com>
2026-04-30 21:24:58 +0700

    Added serializers.py to restaurant

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Menu
+from .serializers import UserSerializer, MenuSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ViewSet
+from rest_framework import generics
+
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
