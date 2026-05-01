9d54e5634778eb6f8b01852b44c1f998410aaa46
Dareach Hoeurn <dareachhoeurn@gmail.com>
2026-05-01 09:43:55 +0700

    added permissions and obtain_auth_token view

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('reservation.urls')),
     path('',include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
